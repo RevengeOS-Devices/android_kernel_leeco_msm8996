da4ebce730e4cb09dcc4eed453c958439bab1fcb
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2014-10-21 12:50:04 -0700

    rcu: Remove "cpu" argument to rcu_note_context_switch()
    The "cpu" argument to rcu_note_context_switch() is always the current
    CPU, so drop it.  This in turn allows the "cpu" argument to
    rcu_preempt_note_context_switch() to be removed, which allows the sole
    use of "cpu" in both functions to be replaced with a this_cpu_ptr().
    Again, the anticipated cross-CPU uses of these functions has been
    replaced by NO_HZ_FULL.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Pranith Kumar <bobby.prani@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3250,7 +3250,7 @@ static void __sched __schedule(void)
 	preempt_disable();
 	cpu = smp_processor_id();
 	rq = cpu_rq(cpu);
-	rcu_note_context_switch(cpu);
+	rcu_note_context_switch();
 	prev = rq->curr;
 
 	/*
