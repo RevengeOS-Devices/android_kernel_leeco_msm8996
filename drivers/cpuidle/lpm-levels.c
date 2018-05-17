2c377456d4981eb415dd24a13f17e0b1d3d483a3
Sultan Alsawaf <sultanxda@gmail.com>
2018-05-16 22:34:25 -0700

    cpuidle: lpm-levels: Remove sleep_disabled switch
    This is only used to keep cpuidle disabled during boot, which is
    unnecessary and doesn't really improve boot times (but does incur a high
    power cost).
    
    In order to reduce the power consumed while booting the device, don't
    allow cpuidle to be disabled.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: utsavisgreat <utsavbalar1231@gmail.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -153,10 +153,6 @@ module_param_named(
 	print_parsed_dt, print_parsed_dt, bool, S_IRUGO | S_IWUSR | S_IWGRP
 );
 
-static bool sleep_disabled;
-module_param_named(sleep_disabled,
-	sleep_disabled, bool, S_IRUGO | S_IWUSR | S_IWGRP);
-
 s32 msm_cpuidle_get_deep_idle_latency(void)
 {
 	return 10;
@@ -709,9 +705,6 @@ static int cpu_power_select(struct cpuidle_device *dev,
 	uint32_t next_wakeup_us = sleep_us;
 	uint32_t *min_residency = get_per_cpu_min_residency(dev->cpu);
 	uint32_t *max_residency = get_per_cpu_max_residency(dev->cpu);
-
-	if (sleep_disabled || sleep_us  < 0)
-		return 0;
 
 	idx_restrict = cpu->nlevels + 1;
 
