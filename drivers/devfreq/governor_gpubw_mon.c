07f4a3a5400bb4c3e3c0bc27bd8a5992e814d227
sdv22 <sdv22@live.ru>
2019-02-08 05:57:44 +0300

    gpubw: Guard devfreq null pointer dereference

--- drivers/devfreq/governor_gpubw_mon.c
+++ drivers/devfreq/governor_gpubw_mon.c
@@ -196,6 +196,8 @@ static int devfreq_gpubw_event_handler(struct devfreq *devfreq,
 	int result = 0;
 	unsigned long freq;
 
+    if( devfreq == 0 ) return result;
+
 	mutex_lock(&devfreq->lock);
 	freq = devfreq->previous_freq;
 	switch (event) {
@@ -212,9 +214,11 @@ static int devfreq_gpubw_event_handler(struct devfreq *devfreq,
 	case DEVFREQ_GOV_SUSPEND:
 		{
 			struct devfreq_msm_adreno_tz_data *priv = devfreq->data;
-			priv->bus.total_time = 0;
-			priv->bus.gpu_time = 0;
-			priv->bus.ram_time = 0;
+            if( priv != 0 ) {
+    			priv->bus.total_time = 0;
+    			priv->bus.gpu_time = 0;
+    			priv->bus.ram_time = 0;
+            }
 		}
 		break;
 	default:
